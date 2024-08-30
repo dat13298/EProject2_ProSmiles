7207b1e41d45b170d6450fc580340b54bb5c6db2
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-08-30 23:19:19 +0700

    update FXML Home

--- src/main/java/com/aptech/eproject2_prosmiles/Service/AuthenticationService.java
+++ src/main/java/com/aptech/eproject2_prosmiles/Service/AuthenticationService.java
@@ -3,11 +3,10 @@ package com.aptech.eproject2_prosmiles.Service;
 import com.aptech.eproject2_prosmiles.Global.AppProperties;
 import com.aptech.eproject2_prosmiles.Model.Entity.Staff;
 import com.aptech.eproject2_prosmiles.Repository.StaffDAO;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 public class AuthenticationService {
@@ -15,8 +14,8 @@ public class AuthenticationService {
     /*LOGIN*/
     public static boolean login(Staff staff) {
         //check valid
-        Staff staffLogged = StaffDAO.validateLogin(staff);
-        return true;
+        Staff staffLogged = StaffDAO.getStaffByPhoneOrEmail(staff);
+        return BCrypt.checkpw(staff.getPassword(), staffLogged.getPassword());
     }
 
     /*REGISTER*/
