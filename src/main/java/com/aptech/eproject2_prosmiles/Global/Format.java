7934ce830a9396f1e2d4baec80b9df4128d52f61
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-01 02:20:08 +0700

    add convert LocalDatetime to String

--- src/main/java/com/aptech/eproject2_prosmiles/Global/Format.java
+++ src/main/java/com/aptech/eproject2_prosmiles/Global/Format.java
@@ -1,5 +1,12 @@
 package com.aptech.eproject2_prosmiles.Global;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Format {
+    public static String formatDate(LocalDateTime date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return date.format(formatter);
+    }
 
 }
