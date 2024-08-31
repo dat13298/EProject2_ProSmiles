ca2ad5bd962a1dc3e1c65f1871f061f178c63810
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-01 02:19:44 +0700

    add validate phone email

--- src/main/java/com/aptech/eproject2_prosmiles/Global/Validation.java
+++ src/main/java/com/aptech/eproject2_prosmiles/Global/Validation.java
@@ -1,4 +1,16 @@
 package com.aptech.eproject2_prosmiles.Global;
 
 public class Validation {
+    public static boolean isPhoneNumberValid(String phoneNumber) {
+        String pattern = "^[0-9]{10,15}";
+        return phoneNumber.matches(pattern);
+    }
+    public static boolean isEmailValid(String email) {
+        String pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$\n";
+//        example matches:
+//        example@example.com
+//        user.name@domain.co.uk
+//        first_last123@sub.domain.org
+        return email.matches(pattern);
+    }
 }
