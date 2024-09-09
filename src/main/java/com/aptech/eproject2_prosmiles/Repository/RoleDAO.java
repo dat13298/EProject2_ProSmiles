8c4bf8409e80c8314f1d55e819d7e3c894bf898b
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-10 00:53:14 +0700

    fix name column db

--- src/main/java/com/aptech/eproject2_prosmiles/Repository/RoleDAO.java
+++ src/main/java/com/aptech/eproject2_prosmiles/Repository/RoleDAO.java
@@ -18,7 +18,7 @@ public class RoleDAO implements DentalRepository<Role> {
     public ObservableList<Role> getAll() {
         ObservableList<Role> listRole = FXCollections.observableArrayList();
         try {
-            String sql = "SELECT r.id, r.tile, r.slug, r.description, r.is_deleted, r.create_at, r.update_at " +
+            String sql = "SELECT r.id, r.title, r.slug, r.description, r.is_deleted, r.created_at, r.updated_at " +
                     "FROM role r WHERE 1=1 LIMIT 100";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
@@ -37,7 +37,7 @@ public class RoleDAO implements DentalRepository<Role> {
     @Override
     public Role getById(int id) {
         try {
-            String sql = "SELECT r.id, r.tile, r.slug, r.description, r.is_deleted, r.create_at, r.update_at " +
+            String sql = "SELECT r.id, r.title, r.slug, r.description, r.is_deleted, r.created_at, r.updated_at " +
                     "FROM role r WHERE r.id=?";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
             preparedStatement.setInt(1, id);
@@ -72,7 +72,7 @@ public class RoleDAO implements DentalRepository<Role> {
     @Override
     public Role save(Role entity) {
         try {
-            String sql = "INSERT INTO role (title, slug, description, create_at) " +
+            String sql = "INSERT INTO role (title, slug, description, created_at) " +
                     "VALUES (?, ?, ?, ?)";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, entity.getTitle());
@@ -91,7 +91,7 @@ public class RoleDAO implements DentalRepository<Role> {
     public Role update(Role entity) {
         try {
             String sql = "UPDATE role r " +
-                    "SET r.title = ?, r.slug = ?, r.description = ?, r.update_at = ? " +
+                    "SET r.title = ?, r.slug = ?, r.description = ?, r.updated_at = ? " +
                     "WHERE r.id = ?";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setString(1, entity.getTitle());
@@ -113,7 +113,7 @@ public class RoleDAO implements DentalRepository<Role> {
         boolean result = false;
         try {
             String sql = "UPDATE role r " +
-                    "SET r.update_at = ? , r.is_deleted = ?" +
+                    "SET r.updated_at = ? , r.is_deleted = ?" +
                     "WHERE r.id = ?";
             PreparedStatement pstmt = conn.prepareStatement(sql);
             pstmt.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
@@ -138,9 +138,11 @@ public class RoleDAO implements DentalRepository<Role> {
         Timestamp timestamp = rs.getTimestamp("created_at");
         LocalDateTime createdAt = timestamp != null ? timestamp.toLocalDateTime() : null;
         role.setCreatedAt(createdAt);
-        Timestamp timestamp2 = rs.getTimestamp("updated_at");
-        LocalDateTime updatedAt = timestamp2 != null ? timestamp2.toLocalDateTime() : null;
-        role.setUpdatedAt(updatedAt);
+        if(rs.getTimestamp("updated_at") != null){
+            Timestamp timestamp2 = rs.getTimestamp("updated_at");
+            LocalDateTime updatedAt = timestamp2 != null ? timestamp2.toLocalDateTime() : null;
+            role.setUpdatedAt(updatedAt);
+        }
         return role;
     }
 }
