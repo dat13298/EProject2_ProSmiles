535efc839c85b48287128adcd48b9fa7c5d0ba7d
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-09-10 00:53:42 +0700

    add method get value gender ENUM

--- src/main/java/com/aptech/eproject2_prosmiles/Model/Enum/EGender.java
+++ src/main/java/com/aptech/eproject2_prosmiles/Model/Enum/EGender.java
@@ -9,4 +9,12 @@ public enum EGender {
     public String getGender() {
         return gender;
     }
+    public static EGender fromValue(String value) {
+        for (EGender e : EGender.values()) {
+            if (e.gender.equals(value)) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
