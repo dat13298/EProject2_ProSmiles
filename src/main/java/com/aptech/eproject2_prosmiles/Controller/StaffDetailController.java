67f1f034751a01d8b90d2f7e16b5cce9dbd91914
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-10-08 15:01:20 +0700

    add handleCancel for close dialog

--- src/main/java/com/aptech/eproject2_prosmiles/Controller/StaffDetailController.java
+++ src/main/java/com/aptech/eproject2_prosmiles/Controller/StaffDetailController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -40,20 +41,32 @@ public class StaffDetailController extends BaseController{
     private Label lbl_staff_address;
     @FXML
     private Label lbl_age;
+    @FXML
+    private Button btn_edit;
+    @FXML
+    private Button btn_cancel;
+
+    private Staff staff;
+    private Stage dialogStage;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        btn_cancel.setOnAction(event -> dialogStage.close());
+    }
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
     }
 
     public void setStaffDetails(Staff staff) {
+        this.staff = staff;
         lbl_staff_name.setText(staff.getFirstName() + " " + staff.getLastName());
         lbl_staff_email.setText(staff.getEmail());
         lbl_staff_gender.setText(staff.getEGender().getGender());
         lbl_staff_phone_number.setText(staff.getPhone());
         lbl_staff_address.setText(staff.getAddress());
         lbl_age.setText(String.valueOf(staff.getAge()));
-
     }
 }
