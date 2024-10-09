cb266cfea72ab2a41ac95437537c5b1d249d549d
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-10-10 00:32:22 +0700

    open form when click edit

--- src/main/java/com/aptech/eproject2_prosmiles/Controller/StaffDetailController.java
+++ src/main/java/com/aptech/eproject2_prosmiles/Controller/StaffDetailController.java
@@ -3,14 +3,17 @@ package com.aptech.eproject2_prosmiles.Controller;
 import com.aptech.eproject2_prosmiles.Model.Entity.Staff;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class StaffDetailController extends BaseController{
+public class StaffDetailController extends BaseController {
     @FXML
     private Label lbl_staff_name;
     @FXML
@@ -27,15 +30,21 @@ public class StaffDetailController extends BaseController{
     private Button btn_edit;
     @FXML
     private Button btn_cancel;
+    @FXML
+    private ImageView imv_staff_image;
 
     private Staff staff;
     private Stage dialogStage;
+    private StaffListController staffListController;
 
+    public void setStaffListController(StaffListController staffListController) {
+        this.staffListController = staffListController;
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btn_edit.setOnAction(event -> {
-
+            staffListController.showAddEditForm(staff, true);
         });
         btn_cancel.setOnAction(event -> dialogStage.close());
     }
@@ -44,13 +53,22 @@ public class StaffDetailController extends BaseController{
         this.dialogStage = dialogStage;
     }
 
-    public void setStaffDetails(Staff staff) {
-        this.staff = staff;
-        lbl_staff_name.setText(staff.getFirstName() + " " + staff.getLastName());
-        lbl_staff_email.setText(staff.getEmail());
-        lbl_staff_gender.setText(staff.getEGender().getGender());
-        lbl_staff_phone_number.setText(staff.getPhone());
-        lbl_staff_address.setText(staff.getAddress());
-        lbl_age.setText(String.valueOf(staff.getAge()));
+    public void setStaffDetails(Staff staffClicked) {
+        this.staff = staffClicked;
+        lbl_staff_name.setText(staffClicked.getFirstName() + " " + staff.getLastName());
+        lbl_staff_email.setText(staffClicked.getEmail());
+        lbl_staff_gender.setText(staffClicked.getEGender().getGender());
+        lbl_staff_phone_number.setText(staffClicked.getPhone());
+        lbl_staff_address.setText(staffClicked.getAddress());
+        lbl_age.setText(String.valueOf(staffClicked.getAge()));
+
+        File file = new File("src/main/resources" + staffClicked.getImagePath());
+        if (file.exists()) {
+            Image image = new Image(file.toURI().toString());
+            imv_staff_image.setImage(image);
+        } else {
+            System.out.println("Image file not found.");
+        }
+
     }
 }
